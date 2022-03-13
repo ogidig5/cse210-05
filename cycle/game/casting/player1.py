48aee5bdbb29b20d35eb3d11e3290caa77a74d7a
ColdRamen <alexward976@gmail.com>
2022-03-12 22:21:06 -0700

    updating class names

--- cycle/game/casting/player1.py
+++ cycle/game/casting/player1.py
@@ -1,11 +1,17 @@
 import constants
-from game.casting.snake import Snake
 from game.casting.actor import Actor
 from game.shared.point import Point
 
 
-class Food(Actor):
+class Player1(Actor):
+    """
+    A long limbless reptile.
     
+    The responsibility of Snake is to move itself.
+
+    Attributes:
+        _points (int): The number of points the food is worth.
+    """
     def __init__(self):
         super().__init__()
         self._segments = []
@@ -39,21 +45,21 @@ class Food(Actor):
             segment.set_position(position)
             segment.set_velocity(velocity)
             segment.set_text("#")
-            segment.set_color(constants.BLUE)
+            segment.set_color(constants.GREEN)
             self._segments.append(segment)
 
     def turn_head(self, velocity):
         self._segments[0].set_velocity(velocity)
     
     def _prepare_body(self):
-        x = 300
-        y = 100
+        x = int(constants.MAX_X / 2)
+        y = int(constants.MAX_Y / 2)
 
         for i in range(constants.SNAKE_LENGTH):
             position = Point(x - i * constants.CELL_SIZE, y)
             velocity = Point(1 * constants.CELL_SIZE, 0)
             text = "8" if i == 0 else "#"
-            color = constants.YELLOW if i == 0 else constants.BLUE
+            color = constants.YELLOW if i == 0 else constants.GREEN
             
             segment = Actor()
             segment.set_position(position)
--- cycle/game/casting/player2.py
+++ cycle/game/casting/player2.py
@@ -3,15 +3,8 @@ from game.casting.actor import Actor
 from game.shared.point import Point
 
 
-class Snake(Actor):
-    """
-    A long limbless reptile.
+class Player2(Actor):
     
-    The responsibility of Snake is to move itself.
-
-    Attributes:
-        _points (int): The number of points the food is worth.
-    """
     def __init__(self):
         super().__init__()
         self._segments = []
@@ -45,21 +38,21 @@ class Snake(Actor):
             segment.set_position(position)
             segment.set_velocity(velocity)
             segment.set_text("#")
-            segment.set_color(constants.GREEN)
+            segment.set_color(constants.BLUE)
             self._segments.append(segment)
 
     def turn_head(self, velocity):
         self._segments[0].set_velocity(velocity)
     
     def _prepare_body(self):
-        x = int(constants.MAX_X / 2)
-        y = int(constants.MAX_Y / 2)
+        x = 300
+        y = 100
 
         for i in range(constants.SNAKE_LENGTH):
             position = Point(x - i * constants.CELL_SIZE, y)
             velocity = Point(1 * constants.CELL_SIZE, 0)
             text = "8" if i == 0 else "#"
-            color = constants.YELLOW if i == 0 else constants.GREEN
+            color = constants.YELLOW if i == 0 else constants.BLUE
             
             segment = Actor()
             segment.set_position(position)
